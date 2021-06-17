d81d90a8f05b426bb554de3bd49e3924ae182c8d
kristellu <kristellu@uninorte.edu.co>
2021-06-17 00:51:00 -0500

    added: details login and routes init

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,32 +5,77 @@ import { LoginComponent } from './login/login.component';
 import { AuthGuard } from './auth.guard';
 
 export const AppRoutes: Routes = [
-    {
+  {
+    path: '',
+    component: FullComponent,
+    canActivate: [AuthGuard],
+    children: [
+      {
         path: '',
-        component: FullComponent,
-        canActivate: [AuthGuard],
-        children: [
-            {
-                path: '',
-                redirectTo: '/login',
-                pathMatch: 'full'
-            },
-            {
-                path: 'material',
-                loadChildren: () => import('./material-component/material.module').then(m => m.MaterialComponentsModule)
-            },
-            {
-                path: 'starter',
-                loadChildren: () => import('./starter/starter.module').then(m => m.StarterModule)
-            },
-            {
-                path: 'icons',
-                loadChildren: () => import('./icons/mat-icon.module').then(m => m.IconsModule)
-            }
-        ]
-    },
-    {
-        path: 'login',
-        component: LoginComponent,
-    }
+        redirectTo: '/login',
+        pathMatch: 'full',
+      },
+      {
+        path: 'material',
+        loadChildren: () =>
+          import('./material-component/material.module').then(
+            (m) => m.MaterialComponentsModule
+          ),
+      },
+      {
+        path: 'starter',
+        loadChildren: () =>
+          import('./starter/starter.module').then((m) => m.StarterModule),
+      },
+      {
+        path: 'icons',
+        loadChildren: () =>
+          import('./icons/mat-icon.module').then((m) => m.IconsModule),
+      },
+    ],
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'signup',
+    component: LoginComponent,
+  },
+  {
+    path: 'dashboard',
+    component: LoginComponent,
+  },
+  {
+    path: 'authorization',
+    component: LoginComponent,
+  },
+  {
+    path: 'frequentlyQuestions',
+    component: LoginComponent,
+  },
+  {
+    path: 'contacts',
+    component: LoginComponent,
+  },
+  {
+    path: 'glossary',
+    component: LoginComponent,
+  },
+  {
+    path: 'patners',
+    component: LoginComponent,
+  },
+  {
+    path: 'covenants',
+    component: LoginComponent,
+  },
+  {
+    path: 'nominations',
+    component: LoginComponent,
+  },
+  {
+    path: 'applications',
+    component: LoginComponent,
+  },
 ];
